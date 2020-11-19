73dd806f0863bf4fa0fc4079baa875a3a647fb97
rianma <mmg9403@gmail.com>
2020-11-19 14:02:54 +0800

    feat: 修改堆叠柱状图的示例数据

--- src/bar-chart/stacked-bar-chart.js
+++ src/bar-chart/stacked-bar-chart.js
@@ -2,26 +2,42 @@ import { Chart } from '@antv/g2'
 
 // c2: 堆积柱状图
 const c2data = [
-  { genre: 'Sports', country: 'CN', boxOffice: 120 },
-  { genre: 'Sports', country: 'US', boxOffice: 1000 },
-  { genre: 'Sports', country: 'JP', boxOffice: 30 },
-  { genre: 'Strategy', country: 'CN', boxOffice: 130 },
-  { genre: 'Strategy', country: 'US', boxOffice: 463 },
-  { genre: 'Strategy', country: 'JP', boxOffice: 2200 },
-  { genre: 'Action', country: 'CN', boxOffice: 150 },
-  { genre: 'Action', country: 'US', boxOffice: 800 },
-  { genre: 'Action', country: 'JP', boxOffice: 37 },
-  { genre: 'Shooter', country: 'CN', boxOffice: 178 },
-  { genre: 'Shooter', country: 'US', boxOffice: 600 },
-  { genre: 'Shooter', country: 'JP', boxOffice: 30 },
-  { genre: 'Other', country: 'CN', boxOffice: 234 },
-  { genre: 'Other', country: 'US', boxOffice: 220 },
-  { genre: 'Other', country: 'JP', boxOffice: 3000 },
+  { genre: 'Sports', country: 'CN', sold: 120 },
+  { genre: 'Sports', country: 'US', sold: 1000 },
+  { genre: 'Sports', country: 'JP', sold: 30 },
+
+  { genre: 'Strategy', country: 'CN', sold: 130 },
+  { genre: 'Strategy', country: 'US', sold: 463 },
+  { genre: 'Strategy', country: 'JP', sold: 2200 },
+
+  { genre: 'Action', country: 'CN', sold: 150 },
+  { genre: 'Action', country: 'US', sold: 800 },
+  { genre: 'Action', country: 'JP', sold: 37 },
+
+  { genre: 'Shooter', country: 'CN', sold: 178 },
+  { genre: 'Shooter', country: 'US', sold: 600 },
+  { genre: 'Shooter', country: 'JP', sold: 30 },
+
+  { genre: 'Table', country: 'CN', sold: 334 },
+  { genre: 'Table', country: 'US', sold: 2394 },
+  { genre: 'Table', country: 'JP', sold: 123 },
+
+  { genre: 'Adventure', country: 'CN', sold: 656 },
+  { genre: 'Adventure', country: 'US', sold: 432 },
+  { genre: 'Adventure', country: 'JP', sold: 1904 },
+
+  { genre: 'Role Playing', country: 'CN', sold: 656 },
+  { genre: 'Role Playing', country: 'US', sold: 432 },
+  { genre: 'Role Playing', country: 'JP', sold: 1904 },
+
+  { genre: 'Race', country: 'CN', sold: 234 },
+  { genre: 'Race', country: 'US', sold: 220 },
+  { genre: 'Race', country: 'JP', sold: 3000 },
 ]
 
 const chart2 = new Chart({
   container: 'c2',
-  width: 600,
+  forceFit: true,
   height: 300
 })
 
@@ -39,16 +55,16 @@ chart2.source(c2data.map(item => {
 }))
 
 // 设置纵轴字段
-chart2.scale('boxOffice', { nice: true })
+chart2.scale('sold', { nice: true })
 
 // 设置为直方图格式，并且以 genre 为x轴
 chart2.interval()
-  .position('genre*boxOffice')
+  .position('genre*sold')
   .color('country')
   .adjust('stack')
 
 /**
- * 对于复杂场景，需要变更数据的，就用监听事件的方式，获取 items 再修改
+ * 自定义 tooltip，对于复杂场景，需要变更数据的，就用监听事件的方式，获取 items 再修改
  * @see https://g2-v3.antv.vision/zh/docs/manual/tutorial/tooltip#%E6%A0%BC%E5%BC%8F%E5%8C%96-tooltip-%E7%9A%84%E6%98%BE%E7%A4%BA%E5%86%85%E5%AE%B9
  **/ 
 chart2.on('tooltip:change', (event) => {
@@ -62,7 +78,5 @@ chart2.on('tooltip:change', (event) => {
   }
   items.push(totalItem)
 })
-
-// 使用 .tooltip() API 以显示 total 总数的值
 
 chart2.render()
